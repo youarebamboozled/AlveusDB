bbe1bd7f769b129bd6bc83cbc01853e9c4333c8c
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-03-23 20:44:04 +0100

    Update http_handler.rs

--- src/http_handler.rs
+++ src/http_handler.rs
@@ -5,25 +5,35 @@ use crate::{error, debug, info, warn, verbose, fatal};
 use crate::http_response::{HttpResponse};
 use crate::http_header::HttpHeader;
 use std::time::Instant;
+use crate::json::{Query, QueryResultType, read, write};
+use std::io::ErrorKind;
 
 
-pub(crate) fn handle_request(mut stream: TcpStream) {
+pub(crate) fn handle_request(mut stream: &TcpStream) {
     let start = Instant::now();
-    // get the path from the request
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    let mut first_buffer = [0; 256];
+    stream.read(&mut first_buffer).unwrap();
+
+    // get the content length from the request and read the rest of the request
+    let content_length = String::from_utf8_lossy(&first_buffer[..]).split("Content-Length: ").collect::<Vec<&str>>()[1].split("\r\n").collect::<Vec<&str>>()[0].parse::<usize>().unwrap();
+    let mut second_buffer = vec![0; content_length];
+    debug!("Content length: {}", content_length);
+    stream.read(&mut second_buffer).unwrap();
+
+    let mut buffer = first_buffer.to_vec();
+    buffer.append(&mut second_buffer);
 
     // get the path from the request
     let binding = String::from_utf8_lossy(&buffer[..]);
+    //remove every \0 from the string
+    let binding = binding.replace("\0", "");
     let req_method = binding.split(" ").collect::<Vec<&str>>()[0];
     let path = binding.split(" ").collect::<Vec<&str>>()[1];
-    debug!("Path: {}", path);
-    debug!("Method: {}", req_method);
 
     // route the request to the correct handler
     let response = match path {
         "/" => index(),
-        "/sql" => db_handler(binding),
+        "/sql" => db_handler(binding.clone()),
         _ => unknown_path(),
     };
 
@@ -31,18 +41,19 @@ pub(crate) fn handle_request(mut stream: TcpStream) {
     let end = Instant::now();
     let duration = end.duration_since(start);
     debug!("Request took {}µs", duration.as_micros());
-    let response = response.to_string();
-    stream.write(response.as_bytes()).unwrap();
+    stream.write(response.to_string().as_bytes()).unwrap();
     stream.flush().unwrap();
+    // log the request in this format:
+    // [IP] [METHOD] [PATH] [STATUS CODE] [TIME]
+    let ip = stream.peer_addr().unwrap().ip().to_string();
+    info!("{} {} {} {} {}", ip, req_method, path, response.status_code.to_string(),
+        response.content.split("time\": \"").collect::<Vec<&str>>()[1].split("\"").collect::<Vec<&str>>()[0]);
 }
 
-fn db_handler(binding: Cow<str>) -> HttpResponse {
-    debug!("Request: {}", binding);
+fn db_handler(binding: String) -> HttpResponse {
     let req_method = binding.split(" ").collect::<Vec<&str>>()[0];
     let header = binding.split("\r\n\r\n").collect::<Vec<&str>>()[0];
-    debug!("Header: {}", header);
     let body = binding.split("\r\n\r\n").collect::<Vec<&str>>()[1];
-    debug!("Body: {}", body);
 
     let response = match req_method {
         "GET" => db_get_handler(body, header),
@@ -54,26 +65,77 @@ fn db_handler(binding: Cow<str>) -> HttpResponse {
 }
 
 fn db_get_handler(body: &str, header: &str) -> HttpResponse {
-    debug!("Body: {}", body);
-    debug!("Header: {}", header);
     let mut response = HttpResponse::new();
     response.protocol = "HTTP/1.1".to_string();
     response.status_code = 200;
     response.header = HttpHeader::new()
-        .with_content_type("text/html".to_string());
-    response.content = format!("Connecting to database: {} and table: {}", "test", "test");
+        .with_content_type("application/json".to_string());
+    // get the database and table from the request header which are in the form of:
+    // DB: test
+    // TABLE: test
+    if !header.contains("DB: ") || !header.contains("TABLE: ") {
+        response.status_code = 400;
+        response.content = r#"{"error": "Bad Request. Missing or malformed DB or TABLE header"}"#.to_string();
+        return response;
+    }
+    let database = String::from(header.split("DB: ").collect::<Vec<&str>>()[1].split("\r\n").collect::<Vec<&str>>()[0]);
+    let table = String::from(header.split("TABLE: ").collect::<Vec<&str>>()[1].split("\r\n").collect::<Vec<&str>>()[0]);
+    let query = Query {
+        database,
+        table,
+        content: None,
+    };
+    let time = Instant::now();
+    let result = read(&query);
+    let duration = time.elapsed();
+    let time = duration.as_micros();
+    match result.status {
+        QueryResultType::Success => {
+            response.content = r#"{"time": ""#.to_string() + &time.to_string() + r#"μs", "data": "# + &result.query.content.unwrap() + r#"}"#;
+        },
+        QueryResultType::Error => {
+            response.status_code = 500;
+            response.content = r#"{"error": ""#.to_string() + &result.message + r#""}"#;
+        },
+    }
+
     response
 }
 
 fn db_post_handler(body: &str, header: &str) -> HttpResponse {
-    debug!("Body: {}", body);
-    debug!("Header: {}", header);
     let mut response = HttpResponse::new();
     response.protocol = "HTTP/1.1".to_string();
     response.status_code = 200;
     response.header = HttpHeader::new()
-        .with_content_type("text/html".to_string());
-    response.content = format!("Connecting to database: {} and table: {}", "test", "test");
+        .with_content_type("application/json".to_string());
+
+    if !header.contains("DB: ") || !header.contains("TABLE: ") {
+        response.status_code = 400;
+        response.content = r#"{"error": "Bad Request. Missing or malformed DB or TABLE header"}"#.to_string();
+        return response;
+    }
+
+    let database = String::from(header.split("DB: ").collect::<Vec<&str>>()[1].split("\r\n").collect::<Vec<&str>>()[0]);
+    let table = String::from(header.split("TABLE: ").collect::<Vec<&str>>()[1].split("\r\n").collect::<Vec<&str>>()[0]);
+    let query = Query {
+        database,
+        table,
+        content: Some(body.to_string()),
+    };
+    let time = Instant::now();
+    let result = write(&query);
+    let duration = time.elapsed();
+    let time = duration.as_micros();
+    match result.status {
+        QueryResultType::Success => {
+            response.content = r#"{"time": ""#.to_string() + &time.to_string() + r#"μs", "data": "# + &result.query.content.unwrap() + r#"}"#;
+        },
+        QueryResultType::Error => {
+            response.status_code = 500;
+            response.content = r#"{"error": ""#.to_string() + &result.message + r#""}"#;
+        },
+    }
+
     response
 }
 
