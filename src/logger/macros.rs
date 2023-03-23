6f74ba0c52a0caaef7390b64d7fe0bf703ca9e23
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-03-23 12:23:58 +0100

    Update macros.rs

--- src/logger/macros.rs
+++ src/logger/macros.rs
@@ -2,7 +2,7 @@
 macro_rules! info {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Info {
-            println!("{} \x1B[32;1;1m[INFO]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[32;1;1m[INFO]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
@@ -11,7 +11,7 @@ macro_rules! info {
 macro_rules! debug {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Debug {
-            println!("{} \x1B[96;1;1m[DEBUG]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[96;1;1m[DEBUG]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
@@ -20,7 +20,7 @@ macro_rules! debug {
 macro_rules! verbose {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Verbose {
-            println!("{} \x1B[35;1;1m[VERBOSE]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[35;1;1m[VERBOSE]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
@@ -29,7 +29,7 @@ macro_rules! verbose {
 macro_rules! warn {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Warn {
-            println!("{} \x1B[33;1;1m[WARN]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[33;1;1m[WARN]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
@@ -38,7 +38,7 @@ macro_rules! warn {
 macro_rules! error {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Error {
-            println!("{} \x1B[31;1;1m[ERROR]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[31;1;1m[ERROR]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
@@ -47,7 +47,7 @@ macro_rules! error {
 macro_rules! fatal {
     ($($arg:tt)*) => ({
         if $crate::logger::log::log_level() <= $crate::logger::log_level::LogLevel::Fatal {
-            println!("{} \x1B[31;1;5;7m[FATAL]\x1B[0m {}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
+            println!("{} \x1B[31;1;5;7m[FATAL]\x1B[0m\t{}", $crate::logger::log::date_and_time(), format_args!($($arg)*));
         }
     })
 }
