1ebea70e15ccb6526e191811e2f249b410b115f1
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-03-23 21:08:11 +0100

    removed unwrap() and replaced with match

--- src/logger/log.rs
+++ src/logger/log.rs
@@ -3,14 +3,26 @@ use chrono;
 
 pub(crate) fn date_and_time() -> String {
     let now = std::time::SystemTime::now();
-    let now = now.duration_since(std::time::UNIX_EPOCH).unwrap();
+    let now = match now.duration_since(std::time::UNIX_EPOCH) {
+        Ok(n) => n,
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    };
     let now = now.as_secs();
     let now = chrono::NaiveDateTime::from_timestamp_opt(now as i64, 0);
-    let now = chrono::DateTime::<chrono::Utc>::from_utc(now.unwrap(), chrono::Utc);
+    let now = chrono::DateTime::<chrono::Utc>::from_utc(match now {
+        Some(n) => n,
+        None => panic!("SystemTime before UNIX EPOCH!"),
+    }, chrono::Utc);
     now.format("%Y-%m-%d %H:%M:%S").to_string()
 }
 
 pub(crate) fn log_level() -> log_level::LogLevel {
-    let log_level = std::env::var("ALVEUS_LOG").unwrap_or("info".to_string());
-    log_level::LogLevel::from_str(&log_level).unwrap()
+    let log_level = match std::env::var("ALVEUS_LOG") {
+        Ok(log_level) => log_level,
+        Err(_) => "info".to_string(),
+    };
+    match log_level::LogLevel::from_str(&log_level) {
+        Ok(log_level) => log_level,
+        Err(_) => log_level::LogLevel::Info,
+    }
 }
